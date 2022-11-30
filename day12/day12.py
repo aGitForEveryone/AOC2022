0b7d42f4abb94321c396ffb2cd883d1e252a978a
Tobias Van Damme <tobias.damme@skf.com>
2022-11-30 23:19:33 +0100

    Initialized code for each day

---
+++ day12/day12.py
@@ -0,0 +1,34 @@
+import re
+
+from aocd import get_data, submit
+import numpy as np
+
+data = get_data(day=12, year=2022)
+lines = data.splitlines()
+numbers = [int(x) for x in re.findall("(-?\d+)", data)]
+
+
+def part1():
+    """Advent of code 2022 day 12 - Part 1"""
+    answer = 0
+
+    print(f"Solution day 12, part 1: {answer}")
+    return answer
+
+
+def part2():
+    """Advent of code 2022 day 12 - Part 2"""
+    answer = 0
+
+    print(f"Solution day 12, part 2: {answer}")
+    return answer
+
+
+if __name__ == "__main__":
+    part = "a"
+    # part = "b"
+    submit_answer = False
+
+    aocd_result = part1() if part == "a" else part2()
+    if submit_answer:
+        submit(aocd_result, part=part, day=12, year=2022)
