a40207766847350f2a20ee5deeacea5a9dabd5bd
Tobias Van Damme <tobias.damme@skf.com>
2022-12-06 09:02:56 +0100

    Cleaned up code for day 6

--- day06/day6.py
+++ day06/day6.py
@@ -1,14 +1,18 @@
-import re
+from enum import Enum
 
 from aocd import get_data, submit
-import numpy as np
 
 
-def parse_data(load_test_data: bool = False):
+class MarkerType(Enum):
+    PACKET = 4
+    MESSAGE = 14
+
+
+def parse_data(load_test_data: bool = False) -> str:
     """Parser function to parse today's data
 
     Args:
-        load_test_data:     Set to true to load test data from the local 
+        load_test_data:     Set to true to load test data from the local
                             directory
     """
     if load_test_data:
@@ -22,23 +26,42 @@ def parse_data(load_test_data: bool = False):
     return data
 
 
-def find_end_of_start_packet(message: str, num_distinct_characters: int = 4) -> int:
-    """Find the index of the end of the start indicator of the message. The
-    start indicator is the first 4 consecutive characters that are all different
+def find_end_of_marker(message: str, marker_type: MarkerType) -> int:
+    """Find the index of the end of the first occurrence of the specified marker
+    in the specified message. A marker is a set of consecutive character that
+    are all distinct. The length of the marker is given by the marker type.
     """
-    target_index = num_distinct_characters - 1
-    while len(set(message[target_index - (num_distinct_characters - 1): target_index + 1])) < num_distinct_characters:
+    if marker_type not in MarkerType:
+        raise ValueError(
+            f"Unexpected marker types passed. "
+            f"Got {marker_type}, expected one of {list(MarkerType)}"
+        )
+    required_distinct_characters = marker_type.value
+    target_index = required_distinct_characters - 1
+    # Get unique character count in the string of length defined by the marker_type.
+    # Continue parsing new characters until we encounter such a string
+    while (
+        len(
+            set(
+                message[
+                    target_index - (required_distinct_characters - 1) : target_index + 1
+                ]
+            )
+        )
+        < required_distinct_characters
+    ):
         target_index += 1
         if target_index == len(message):
-            raise ValueError(f'Reached end of string without finding the start'
-                             f'package')
+            # Failsafe for the case that we can't find the required marker
+            raise ValueError(
+                f"Reached end of stream without finding the marker of type: {marker_type}"
+            )
     return target_index
 
 
 def part1(data):
     """Advent of code 2022 day 6 - Part 1"""
-    answer = find_end_of_start_packet(data) + 1
-    print(data)
+    answer = find_end_of_marker(data, MarkerType.PACKET) + 1
 
     print(f"Solution day 6, part 1: {answer}")
     return answer
@@ -46,7 +69,7 @@ def part1(data):
 
 def part2(data):
     """Advent of code 2022 day 6 - Part 2"""
-    answer = find_end_of_start_packet(data, 14) + 1
+    answer = find_end_of_marker(data, MarkerType.MESSAGE) + 1
 
     print(f"Solution day 6, part 2: {answer}")
     return answer
@@ -54,13 +77,13 @@ def part2(data):
 
 def main(parts: str, should_submit: bool = False, load_test_data: bool = False) -> None:
     """Main function for solving the selected part(s) of today's puzzle
-    and automatically submitting the answer. 
+    and automatically submitting the answer.
 
     Args:
         parts:          "a", "b", or "ab". Execute the chosen parts
         should_submit:  Set to True if you want to submit your answer
         load_test_data: Set to True if you want to load test data instead of
-                        the full input. By default, this will load the file 
+                        the full input. By default, this will load the file
                         called 'input6.1'
     """
     data = parse_data(load_test_data=load_test_data)
@@ -80,8 +103,8 @@ def main(parts: str, should_submit: bool = False, load_test_data: bool = False) 
 if __name__ == "__main__":
     test_data = False
     # test_data = True
-    # submit_answer = False
-    submit_answer = True
+    submit_answer = False
+    # submit_answer = True
     # main("a", should_submit=submit_answer, load_test_data=test_data)
-    main("b", should_submit=submit_answer, load_test_data=test_data)
-    # main("ab", should_submit=submit_answer, load_test_data=test_data)
+    # main("b", should_submit=submit_answer, load_test_data=test_data)
+    main("ab", should_submit=submit_answer, load_test_data=test_data)
