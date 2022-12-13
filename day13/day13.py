d924b467e50e7dd9d1582b70885a2f4c06551a1e
Tobias Van Damme <tobias.damme@skf.com>
2022-12-13 23:09:49 +0100

    Finished and cleaned day 13

--- day13/day13.py
+++ day13/day13.py
@@ -1,10 +1,14 @@
-import re
+import functools
+from enum import Enum
 from typing import Union
 
 from aocd import get_data, submit
-import numpy as np
 
-import helper_functions
+
+class Order(Enum):
+    CORRECT = -1
+    INDECISIVE = 0
+    INCORRECT = 1
 
 
 def parse_data(load_test_data: bool = False) -> list[str]:
@@ -27,46 +31,120 @@ def parse_data(load_test_data: bool = False) -> list[str]:
     return data
 
 
-def int_int_compare(left: int, right: int) -> bool:
+def int_int_compare(left: int, right: int) -> Order:
     """Compare two ints"""
     if left == right:
-        return 0
+        return Order.INDECISIVE
+    elif left < right:
+        return Order.CORRECT
+    else:  # right > left
+        return Order.INCORRECT
 
 
-def compare_list_items(left: Union[list, int], right: Union[list, int]):
-    """Compare two items"""
+def compare_list_items(left: Union[list, int], right: Union[list, int]) -> Order:
+    """Compare two items. If both are int we can do a direct comparison,
+    otherwise we should compare left and right as lists (converting either into
+    a list if they are of type int) - This recursively calls the compare list
+    function again"""
     if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return True
-        elif left > right:
-            return False
-        # when left_item == right_item, continue
+        status = int_int_compare(left, right)
+    else:
+        # If either left or right is an int, we make it into a list and then
+        # compare two lists
+        if isinstance(left, int):
+            left = [left]
+        if isinstance(right, int):
+            right = [right]
+        status = lists_in_right_order(left, right)
+    return status
 
 
-def lists_in_right_order(left: list, right: list) -> bool:
-    """Check if the lists are in the correct order"""
+def lists_in_right_order(left: list, right: list) -> Order:
+    """Check if lists left and right are in the correct order"""
     for left_item, right_item in zip(left, right):
-        result = compare_list_items(left_item, right_item)
+        status = compare_list_items(left_item, right_item)
+        if status != Order.INDECISIVE:
+            return status
+    else:
+        # If we reached the end of the for-loop, then we should check which list
+        # ran out of items first
+        return int_int_compare(len(left), len(right))
 
 
-def process_pairs(pairs: list[str]):
-    """Compare the lists in each pair"""
-    for pair in pairs:
-        left, right = tuple(map(lambda packet: eval(packet), pair.split()))
+def process_pairs(pairs: list[str]) -> list[int]:
+    """Compare the lists in each pair and return the index of the pairs that
+    are in the correct order"""
+    idx_correctly_ordered_pairs = []
+    for idx, pair in enumerate(pairs):
+        left, right = tuple(map(eval, pair.split()))
+        status = lists_in_right_order(left, right)
+        if status == Order.CORRECT:
+            idx_correctly_ordered_pairs += [idx]
+    return idx_correctly_ordered_pairs
 
 
 def part1(data: list[str]) -> int:
     """Advent of code 2022 day 13 - Part 1"""
-    answer = 0
-    print(data)
+    correctly_ordered_pairs = process_pairs(data)
+    answer = sum(correctly_ordered_pairs) + len(correctly_ordered_pairs)
 
     print(f"Solution day 13, part 1: {answer}")
     return answer
 
 
-def part2(data):
+def lists_in_right_order_int(left: list, right: list) -> int:
+    """Extracts the int value from the enum entry, since the compare function
+    needs an int"""
+    return lists_in_right_order(left, right).value
+
+
+def sort_all_packets(packets: list[list]) -> list[list]:
+    """Sort all the packets in ascending order according to the established
+    list comparison rules. Uses the built-in sorted function with custom compare
+    function.
+
+    Since we can only compare elements relative to each other to sort the list,
+    we utilize functools.cmp_to_key to make our custom compare function work
+    with the key parameter of sorted."""
+    return sorted(packets, key=functools.cmp_to_key(lists_in_right_order_int))
+
+
+def part2(data: list[str]) -> int:
     """Advent of code 2022 day 13 - Part 2"""
-    answer = 0
+    # concatenate the data again so that every packet is on a separate line,
+    # then split the lines to get the individual packets, then convert the
+    # packets to lists using eval.
+    data = list(map(eval, "\n".join(data).splitlines()))
+    sorted_packets = sort_all_packets(data)
+
+    # Add the first divider package
+    divider_packet_1 = [[2]]
+    for idx, packet in enumerate(sorted_packets):
+        if lists_in_right_order(divider_packet_1, packet) == Order.CORRECT:
+            divider_1_idx = idx
+            sorted_packets.insert(idx, divider_packet_1)
+            break
+    else:
+        # if we are at the end of the list, then the packets should be appended
+        # to the end
+        divider_1_idx = len(sorted_packets)
+
+    # Starting from where the first divider packet was inserted, find the spot
+    # to insert the second divider packet (which is larger than the first)
+    divider_packet_2 = [[6]]
+    for idx, packet in enumerate(sorted_packets[divider_1_idx + 1 :]):
+        if lists_in_right_order(divider_packet_2, packet) == Order.CORRECT:
+            # idx counts from start of slice and not from the start of the
+            # sorted list, so we need to correct the value.
+            divider_2_idx = idx + divider_1_idx + 1
+            sorted_packets.insert(idx + divider_1_idx + 1, divider_packet_2)
+            break
+    else:
+        # if we are at the end of the list, then the packets should be appended
+        # to the end
+        divider_2_idx = len(sorted_packets)
+
+    answer = (divider_1_idx + 1) * (divider_2_idx + 1)
 
     print(f"Solution day 13, part 2: {answer}")
     return answer
@@ -98,10 +176,10 @@ def main(parts: str, should_submit: bool = False, load_test_data: bool = False) 
 
 
 if __name__ == "__main__":
-    # test_data = False
-    test_data = True
+    test_data = False
+    # test_data = True
     submit_answer = False
     # submit_answer = True
-    main("a", should_submit=submit_answer, load_test_data=test_data)
+    # main("a", should_submit=submit_answer, load_test_data=test_data)
     # main("b", should_submit=submit_answer, load_test_data=test_data)
-    # main("ab", should_submit=submit_answer, load_test_data=test_data)
+    main("ab", should_submit=submit_answer, load_test_data=test_data)
